c4ec560e56d0f6e6897203449b7b6a52233fbfcd
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2023-08-02 06:34:44 +0530

    test successful for file diff

--- src/main/java/dev/kukode/services/diff/DiffService.java
+++ src/main/java/dev/kukode/services/diff/DiffService.java
@@ -1,13 +1,12 @@
 /*
- * Copyright (C) 29/07/23, 1:34 am KUKODE - Kuchuk Boram Debbarma . - All Rights Reserved
+ * Copyright (C) 02/08/23, 6:34 am KUKODE - Kuchuk Boram Debbarma . - All Rights Reserved
  *
  * Unauthorized copying or redistribution of this file in source and binary forms via any medium
  * is strictly prohibited.
  */
 package dev.kukode.services.diff;
 
-import com.github.difflib.DiffUtils;
-import com.github.difflib.patch.*;
+import com.github.difflib.patch.PatchFailedException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,80 +14,114 @@ import java.util.List;
 
 @Service
 public class DiffService {
-    private static Patch<String> parseUnifiedDiff(List<String> diffLines) {
-        //TODO Patience diff algorithm?
-        //So turns out the problem is the diff algorithms, we need to make our own file diff i guess
-        List<AbstractDelta<String>> deltas = new ArrayList<>();
-        List<String> originalChunk = new ArrayList<>();
-        List<String> revisedChunk = new ArrayList<>();
-        int orgStart = -1;
-        int revStart = -1;
-        boolean inOriginalChunk = false;
-        boolean inRevisedChunk = false;
+    private List<String> findLongestCommonSubsequence(List<String> originalLines, List<String> modifiedLines) {
+        int m = originalLines.size();
+        int n = modifiedLines.size();
 
-        for (String line : diffLines) {
-            if (line.startsWith("@@")) {
-                // Start of a new delta chunk
-                if (inRevisedChunk) {
-                    deltas.add(new ChangeDelta<>(new Chunk<>(orgStart, originalChunk), new Chunk<>(revStart, revisedChunk)));
-                    originalChunk.clear();
-                    revisedChunk.clear();
-                }
-                String[] chunks = line.split("[, ]");
-                orgStart = Integer.parseInt(chunks[1].substring(1));
-                int orgSize = Integer.parseInt(chunks[2]);
-                revStart = Integer.parseInt(chunks[4].substring(1));
-                int revSize = Integer.parseInt(chunks[5]);
-                inOriginalChunk = inRevisedChunk = true;
-            } else if (line.startsWith("-")) {
-                // Original content deletion
-                originalChunk.add(line.substring(1));
-            } else if (line.startsWith("+")) {
-                // Revised content addition
-                revisedChunk.add(line.substring(1));
-            } else if (line.startsWith(" ")) {
-                // Unchanged line
-                if (inOriginalChunk) {
-                    originalChunk.add(line.substring(1));
-                } else if (inRevisedChunk) {
-                    revisedChunk.add(line.substring(1));
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (originalLines.get(i - 1).equals(modifiedLines.get(j - 1))) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        // Add the last delta chunk, if any
-        if (inRevisedChunk) {
-            deltas.add(new ChangeDelta<>(new Chunk<>(orgStart, originalChunk), new Chunk<>(revStart, revisedChunk)));
+        List<String> commonLines = new ArrayList<>();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (originalLines.get(i - 1).equals(modifiedLines.get(j - 1))) {
+                commonLines.add(0, originalLines.get(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
         }
 
-        return new Patch<>();
+        return commonLines;
     }
+
+    private List<String> applyPatch(List<String> originalLines, List<String> diffLines) throws PatchFailedException {
+        List<String> modifiedLines = new ArrayList<>();
+        int originalIndex = 0;
+
+        for (String line : diffLines) {
+            if (line.startsWith("-")) {
+                // Original content deletion, skip the line in the original content
+                originalIndex++;
+            } else if (line.startsWith("+")) {
+                // Revised content addition, add the line to the modified content
+                modifiedLines.add(line.substring(1));
+            } else if (line.startsWith(" ")) {
+                // Unchanged line, add the line from original content to the modified content
+                modifiedLines.add(originalLines.get(originalIndex));
+                originalIndex++;
+            }
+        }
+
+        return modifiedLines;
+    }
+
 
     public String getContentFromOriginalNDiff(String originalContent, String diffData) throws PatchFailedException {
         // Convert the original content to a list of lines
         List<String> originalLines = originalContent.lines().toList();
 
-        // Parse the diff data and generate the patch
+        // Parse the diff data and apply the patch to the original content to get the modified content
         List<String> diffLines = diffData.lines().toList();
-        Patch<String> patch = parseUnifiedDiff(diffLines);
-
-        // Apply the patch to the original content to get the modified content
-        List<String> modifiedContentLines = DiffUtils.patch(originalLines, patch);
+        List<String> modifiedContentLines = applyPatch(originalLines, diffLines);
 
         // Join the lines back into a single string
-        return String.join("\n", modifiedContentLines);
+        return String.join("\n", modifiedContentLines).trim();
     }
 
+
     public String generateDiffData(String originalContent, String modifiedContent) {
-        var originalToken = originalContent.lines().toList();
-        var modifiedToken = modifiedContent.lines().toList();
-        var patch = DiffUtils.diff(originalToken, modifiedToken);
-        StringBuilder diffData = new StringBuilder();
-        for (AbstractDelta<String> delta : patch.getDeltas()) {
-            diffData.append(delta);
-            diffData.append("\n");
+        List<String> originalLines = originalContent.lines().toList();
+        List<String> modifiedLines = modifiedContent.lines().toList();
+
+        List<String> diffLines = new ArrayList<>();
+        List<String> commonLines = findLongestCommonSubsequence(originalLines, modifiedLines);
+
+        int originalIndex = 0;
+        int modifiedIndex = 0;
+
+        for (String commonLine : commonLines) {
+            while (!originalLines.get(originalIndex).equals(commonLine)) {
+                diffLines.add("- " + originalLines.get(originalIndex));
+                originalIndex++;
+            }
+
+            while (!modifiedLines.get(modifiedIndex).equals(commonLine)) {
+                diffLines.add("+ " + modifiedLines.get(modifiedIndex));
+                modifiedIndex++;
+            }
+
+            diffLines.add("  " + commonLine);
+            originalIndex++;
+            modifiedIndex++;
         }
-        System.out.println("Diff data : " + diffData);
-        return diffData.toString();
+
+        while (originalIndex < originalLines.size()) {
+            diffLines.add("- " + originalLines.get(originalIndex));
+            originalIndex++;
+        }
+
+        while (modifiedIndex < modifiedLines.size()) {
+            diffLines.add("+ " + modifiedLines.get(modifiedIndex));
+            modifiedIndex++;
+        }
+
+        return String.join("\n", diffLines);
     }
+
+
 }
--- src/test/java/DiffAlgorithmTest/FileDiffTestClass.java
+++ src/test/java/DiffAlgorithmTest/FileDiffTestClass.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 29/07/23, 1:15 am KUKODE - Kuchuk Boram Debbarma . - All Rights Reserved
+ * Copyright (C) 02/08/23, 6:34 am KUKODE - Kuchuk Boram Debbarma . - All Rights Reserved
  *
  * Unauthorized copying or redistribution of this file in source and binary forms via any medium
  * is strictly prohibited.
@@ -7,6 +7,7 @@
 
 package DiffAlgorithmTest;
 
+import com.github.difflib.patch.DiffException;
 import com.github.difflib.patch.PatchFailedException;
 import dev.kukode.services.diff.DiffService;
 import org.junit.Assert;
@@ -16,38 +17,22 @@ public class FileDiffTestClass {
     DiffService diffService = new DiffService();
 
     @Test
-    public void integrationTestFileDiffONE() throws PatchFailedException {
+    public void integrationTestFileDiffONE() throws DiffException, PatchFailedException {
         String originalContent = "This is the initial Content";
         String firstChange = "This is the first change we made to the content";
-        String secondChange = "Well to the first change we say, get out! we are new here";
-        String thirdChange = "Ok I am tired now lol";
+        String secondChange = "This is the second change and now we are truly testing the chain";
 
-        //Save diffs
+        // Generate the diff data
         String diff1 = diffService.generateDiffData(originalContent, firstChange);
-
         String diff2 = diffService.generateDiffData(firstChange, secondChange);
-        String diff3 = diffService.generateDiffData(secondChange, thirdChange);
 
-        //Load diffs
-        //We get the original diff
-        String firstChangeContent = diffService.getContentFromOriginalNDiff(originalContent, diff1); //Will return us firstChange
+
+        // Load diffs
+        String firstChangeContent = diffService.getContentFromOriginalNDiff(originalContent, diff1);
+        Assert.assertEquals(firstChange, firstChangeContent.trim());
+
         String secondChangeContent = diffService.getContentFromOriginalNDiff(firstChangeContent, diff2);
-        String thirdChangeContent = diffService.getContentFromOriginalNDiff(secondChangeContent, diff3);
-
-        System.out.println("DIFFS==========================");
-        System.out.println(diff1);
-        System.out.println(diff2);
-        System.out.println(diff3);
-
-        System.out.println("RESULTS+++++++++++++++++++");
-        System.out.println(firstChangeContent);
-        System.out.println(secondChangeContent);
-        System.out.println(thirdChangeContent);
-
-        Assert.assertEquals(firstChange, firstChangeContent);
-        Assert.assertEquals(secondChange, secondChangeContent);
-        Assert.assertEquals(thirdChange, thirdChangeContent);
-
-
+        Assert.assertEquals(secondChange, secondChangeContent.trim());
     }
+
 }
